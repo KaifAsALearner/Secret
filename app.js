0e6367cc85ec6a69adc758435ca00bbe048c68f5
KaifAsALearner <s.m.kaif.m.s@gmail.com>
2024-06-22 14:42:37 +0530

    Level 3- Uses Hashing

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express=require("express");
 const mongoose=require("mongoose");
 const bodyParser=require("body-parser");
 const ejs=require("ejs");
-const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 
@@ -16,8 +16,6 @@ const userSchema=new mongoose.Schema({
   email:String,
   password: String
 });
-const secret=process.env.SECRET;
-userSchema.plugin(encrypt,{secret:secret, encryptedFields: ["password"]});
 
 const User=new mongoose.model("User",userSchema);
 
@@ -31,7 +29,7 @@ app.route("/login")
 })
 .post(function(req,res){
   const userName=req.body.username;
-  const password=req.body.password;
+  const password=md5(req.body.password);
   User.findOne({email:userName})
   .then(function(data){
     if(password===data.password){
@@ -53,7 +51,7 @@ app.route("/register")
 .post(function(req,res){
   const newUser=new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save()
   .then(()=>{
